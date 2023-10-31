660882a1e8220f7252c3ee96e8cffe2a003a6916
quill_nichols <leplep00100@gmail.com>
2023-10-30 22:08:35 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+const updateEmployeeWithKeyAndValue = {...employee};
+updateEmployeeWithKeyAndValue[key] = value;
+return updateEmployeeWithKeyAndValue;
+}   
+let employee = {
+    name: "Quilliam Shakespeare",
+    streetAddress: "1234 Main St"
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const updatedEmployee = { ...employee };
+
+    if (key in updatedEmployee) {
+        delete updatedEmployee[key];
+    }
+    return updatedEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    if (key in employee) {
+        delete employee[key];
+    }
+    return employee;
+}
+destructivelyDeleteFromEmployeeByKey(employee, streetAddress);
